6a31d0f929fa54e64cde664b4730bf9f35ef5d68
Alex-Phillip <alexjoelphillip@gmail.com>
2023-04-02 20:34:41 +0100

    practice: useEffect - local storage tasks: data persists across sessions

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import uuid from 'uuid/v4'
+
+const TASKS_STORAGE_KEY = 'TASKS_STORAGE_KEY'
+
+const storeTasks = (taskMap) => {
+  localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(taskMap))
+}
+
+const readStoredTasks = () => {
+  const taskMap = JSON.parse(localStorage.getItem(TASKS_STORAGE_KEY))
+
+  return taskMap ? taskMap : { tasks: [], completedTasks: [] }
+}
 
 function Tasks() {
   const [taskText, setTaskText] = useState('')
-  const [tasks, setTasks] = useState([])
-  const [completedTasks, setCompletedTasks] = useState([])
+
+  const storedTasks = readStoredTasks()
+  const [tasks, setTasks] = useState(storedTasks.tasks)
+  const [completedTasks, setCompletedTasks] = useState(
+    storedTasks.completedTasks
+  )
+
+  useEffect(() => {
+    storeTasks({ tasks, completedTasks })
+  })
 
   const updateTaskText = (event) => {
     setTaskText(event.target.value)
