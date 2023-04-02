1449425e0449d0efd0f8076755ad31aafe984a35
Alex-Phillip <alexjoelphillip@gmail.com>
2023-04-02 09:58:21 +0100

    practice: useState - search widget

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
-export const App = () => {
-  return <div className="App">App</div>
+import { useState } from 'react'
+
+const App = () => {
+  const [userQuery, setUserQuery] = useState('')
+
+  const searchQuery = () => {
+    window.open(`https://google.co.uk/search?q=${userQuery}`, '_blank')
+  }
+  const updateQuery = (event) => {
+    console.log(`User Query: ${userQuery}`)
+    setUserQuery(event.target.value)
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello Alex</h1>
+      <input className="input" onChange={updateQuery} />
+      <button onClick={searchQuery}>Search</button>
+    </div>
+  )
 }
+
+export default App
